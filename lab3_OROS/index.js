81967bfca277985323400c951b3f55577ca6b84a
DanielOrosPNU <146957881+DanielOrosPNU@users.noreply.github.com>
2023-10-15 21:36:07 +0300

    Delete lab3_OROS directory

--- lab3_OROS/index.js
+++
@@ -1,9 +0,0 @@
-function funvid(){
-    const playButton = document.getElementById("buttonhide");
-
-// Скрыть кнопку
-    playButton.style.display = "none";
-    let vid = document.getElementById("myVideo");
-    vid.play()
-    vid.loop()
-}
